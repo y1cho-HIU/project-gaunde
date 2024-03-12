334cca0adc9a987da3f9092947aa35e6cf630253
y1cho <y1cho-HIU>
2024-03-12 16:04:24 +0900

    first commit

---
+++ src/main/java/mincho/projectgaunde/ProjectGaundeApplication.java
@@ -0,0 +1,13 @@
+package mincho.projectgaunde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectGaundeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectGaundeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mincho/projectgaunde/ProjectGaundeApplicationTests.java
@@ -0,0 +1,13 @@
+package mincho.projectgaunde;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectGaundeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
